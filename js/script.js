1bc86799133a4b67448ad54442b10e16ffa45b5a
AntonelloStefania <antonelloste94@gmail.com>
2023-05-18 20:19:12 +0200

    corretto calcolo prezzo over65

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ submit_button.addEventListener('click', function(){
         document.getElementById("carrozza").innerHTML = `carrozza numero: ${carrozza}`;
         document.getElementById("cp").innerHTML = `${cp}`;
     } else if (eta == 'over 65' && km > 0){
-        let price = (km * tariffa) * 0.4;
+        let price = (km * tariffa) * 0.6;
         document.getElementById("nome-utente").innerHTML = `${nome_utente}`;
         document.getElementById("tipo-biglietto").innerHTML = `${eta}`;
         document.getElementById("costo-biglietto").innerHTML = `${price.toFixed(2)}€`;
